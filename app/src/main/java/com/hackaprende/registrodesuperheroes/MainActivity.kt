afb32fbfe108631fbda4f0c3119369d1aee76067
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-03-26 01:09:59 -0300

    :sparkles: App with two activities linked by intent 26.3.2023

--- app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.kt
+++ app/src/main/java/com/hackaprende/registrodesuperheroes/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.hackaprende.registrodesuperheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hackaprende.registrodesuperheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.saveButton.setOnClickListener {
+            openDetailActivity()
+        }
+    }
+
+    private fun openDetailActivity() {
+        // Creamos in intent activity:
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
     }
 }
